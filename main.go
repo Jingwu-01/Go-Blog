4373b259f634d5d887ed12e7a71187c3c7b26376
Jingwu Wang <jingwu@wangjingwudeMacBook-Pro.local>
2023-12-23 14:21:07 +0800

    About page added

--- main.go
+++ main.go
@@ -5,10 +5,19 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
-}
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", handleFunc)
 	http.ListenAndServe(":3000", nil)
+}
+
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL)
+	fmt.Println(r.URL.Path)
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1> Hello, this is go blog. </h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "<p>This is a blog that records my programming notes. Please contact me at <a href=\"mailto:123@rice.edu\">123@rice.edu</a> </p>")
+	} else {
+		fmt.Fprint(w, "<h1> We did not find your request.</h1>")
+	}
 }
