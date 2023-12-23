d1e7b3d808f76b8fb1a3088f4695b5c57809ea0a
Jingwu Wang <jingwu@wangjingwudeMacBook-Pro.local>
2023-12-23 15:10:42 +0800

    serveMux initialized

--- main.go
+++ main.go
@@ -3,21 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", handleFunc)
-	http.ListenAndServe(":3000", nil)
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+	router.HandleFunc("/articles/", articlesHandler)
+
+	http.ListenAndServe(":3000", router)
 }
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.URL)
-	fmt.Println(r.URL.Path)
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1> Hello, this is the updated go blog. </h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "<p>This is a blog that records my programming notes. Please contact me at <a href=\"mailto:123@rice.edu\">123@rice.edu</a> </p>")
 	} else {
+		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1> We did not find your request.</h1>")
 	}
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/about" {
+		fmt.Fprint(w, "<p>This is a blog that records my programming notes. Please contact me at <a href=\"mailto:123@rice.edu\">123@rice.edu</a> </p>")
+	}
+}
+
+func articlesHandler(w http.ResponseWriter, r *http.Request) {
+	id := strings.SplitN(r.URL.Path, "/", 3)[2]
+	fmt.Fprint(w, "Article ID:"+id)
 }
