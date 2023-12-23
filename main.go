74b65fb5cde3374b7d58829e5a7285b8468fefcf
Jingwu Wang <jingwu@wangjingwudeMacBook-Pro.local>
2023-12-23 19:40:39 +0800

    create and store blog initialized

--- main.go
+++ main.go
@@ -3,36 +3,61 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
+var router = mux.NewRouter()
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, this is the updated go blog. </h1>")
+	fmt.Fprintf(w, "Hello, this is the updated go blog. </h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<p>This is a blog that records my programming notes. Please contact me at <a href=\"mailto:123@rice.edu\">123@rice.edu</a> </p>")
+	fmt.Fprintf(w, "<p>This is a blog that records my programming notes. Please contact me at <a href=\"mailto:123@rice.edu\">123@rice.edu</a> </p>")
 
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1> Page is not found </h1>")
+	fmt.Fprintf(w, "<h1> Page is not found </h1>")
 }
 
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-	fmt.Fprint(w, "Article ID:"+id)
+	fmt.Fprintf(w, "Article ID:"+id)
 }
 
 func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Get Article Successful")
+	fmt.Fprintf(w, "Get Article Successful")
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Post Article Successful")
+	fmt.Fprintf(w, "The value of title in r.Form is: %v <br>", r.FormValue("title"))
+	fmt.Fprintf(w, "The value of title in r.PostForm is: %v <br>", r.FormValue("title"))
+
+}
+
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+	html := `
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<title>Create a Tech Blog</title>
+	</head>
+	<body>
+		<form action="%s" method="post">
+			<input type="text" name="title">
+			<textarea name="body" cols="30" rows="10"></textarea>
+			<button type="submit">Sumbit</button>
+		</form>
+	</body>
+	</html>
+	`
+	storeURL, _ := router.Get("articles.store").URL()
+	fmt.Fprintf(w, html, storeURL)
 }
 
 func forceHTMLMiddleware(h http.Handler) http.Handler {
@@ -42,24 +67,27 @@ func forceHTMLMiddleware(h http.Handler) http.Handler {
 	})
 }
 
-func main() {
-	router := mux.NewRouter()
+func removeTrailingSlash(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
+		h.ServeHTTP(w, r)
+	})
+}
 
+func main() {
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods(("GET")).Name("about")
 
 	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
 	router.Use(forceHTMLMiddleware)
 
-	homeURL, _ := router.Get("home").URL()
-	fmt.Println("homeURL: ", homeURL)
-	articleURL, _ := router.Get("articles.show").URL("id", "1")
-	fmt.Println("articleURL: ", articleURL)
-
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", removeTrailingSlash(router))
 }
