87eed40eab90f016a5f753b1712b7b22eb3d37f4
Jingwu Wang <jingwu@wangjingwudeMacBook-Pro.local>
2023-12-23 18:00:17 +0800

    changed to gorilla mux

--- main.go
+++ main.go
@@ -3,36 +3,57 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strings"
+
+	"github.com/gorilla/mux"
 )
 
-func main() {
-	router := http.NewServeMux()
-
-	router.HandleFunc("/", defaultHandler)
-	router.HandleFunc("/about", aboutHandler)
-	router.HandleFunc("/articles/", articlesHandler)
-
-	http.ListenAndServe(":3000", router)
-}
-
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1> Hello, this is the updated go blog. </h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1> We did not find your request.</h1>")
-	}
+	fmt.Fprint(w, "Hello, this is the updated go blog. </h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/about" {
-		fmt.Fprint(w, "<p>This is a blog that records my programming notes. Please contact me at <a href=\"mailto:123@rice.edu\">123@rice.edu</a> </p>")
-	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<p>This is a blog that records my programming notes. Please contact me at <a href=\"mailto:123@rice.edu\">123@rice.edu</a> </p>")
+
 }
 
-func articlesHandler(w http.ResponseWriter, r *http.Request) {
-	id := strings.SplitN(r.URL.Path, "/", 3)[2]
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1> Page is not found </h1>")
+}
+
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
 	fmt.Fprint(w, "Article ID:"+id)
+}
+
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Get Article Successful")
+}
+
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post Article Successful")
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHandler).Methods(("GET")).Name("about")
+
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	homeURL, _ := router.Get("home").URL()
+	fmt.Println("homeURL: ", homeURL)
+	articleURL, _ := router.Get("articles.show").URL("id", "1")
+	fmt.Println("articleURL: ", articleURL)
+
+	http.ListenAndServe(":3000", router)
 }
