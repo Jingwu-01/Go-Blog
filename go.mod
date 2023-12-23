88658c814d334070f94edeb063ba5cea875b33ef
Jingwu Wang <jingwu@wangjingwudeMacBook-Pro.local>
2023-12-23 14:41:50 +0800

    auto reload enabled

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Jingwu-01/goblog
+
+go 1.21.0
--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func handleFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL)
 	fmt.Println(r.URL.Path)
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1> Hello, this is go blog. </h1>")
+		fmt.Fprint(w, "<h1> Hello, this is the updated go blog. </h1>")
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "<p>This is a blog that records my programming notes. Please contact me at <a href=\"mailto:123@rice.edu\">123@rice.edu</a> </p>")
 	} else {
